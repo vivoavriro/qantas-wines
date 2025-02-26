915cf11fa76ba5384fadc362f237293ee5e5db99
Parisa Esmaeil <parisa.esmaeil@qantas.com.au>
2025-02-26 17:51:10 +1100

    add vitest and linter & prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './test/setup.ts',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        '**/*.styles.tsx',
+        '**/*.types.ts',
+        '**/*.index.ts',
+        '**/eslint.config.js',
+        '**/vite.config.ts',
+        '**/vite-env.d.ts',
+        '**/assets/**',
+        '**/dist/**',
+        '**/theme/**',
+        '**/src/main.tsx',
+        '**/src/App.tsx',
+        '**/index-*.js',
+      ],
+    },
+  },
+});
